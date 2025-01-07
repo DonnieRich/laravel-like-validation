6844fda97eb94f406813f7a0c901cb11707a7833
Donnie <donniepask@gmail.com>
2025-01-07 19:35:47 +0100

    [add] main exports

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import Validation from "./Validation.js";
+import ValidationSet from "./ValidationSet.js";
+import Validator from "./Validator.js";
+import BaseRule from "./base/BaseRule.js";
+import BaseValidationSet from "./base/BaseValidationSet.js";
+
+export { Validation, ValidationSet, Validator, BaseRule, BaseValidationSet }
