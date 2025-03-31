d4906f4b3e52eda0cd2624f48d6109a14d9a8b14
Donnie <donniepask@gmail.com>
2025-03-31 19:46:58 +0200

    fixed check for PresentIf rule, added unit test

--- src/rules/PresentIf.ts
+++ src/rules/PresentIf.ts
@@ -3,14 +3,55 @@ import BaseRule from "../base/BaseRule.js";
 class PresentIf extends BaseRule {
     error = "The {field} field must be present if the field {anotherField} has a value of {anotherValue}"
 
-    async validate(data: { [s: string]: any }, field: string, value: Array<string>): Promise<boolean> {
+    async validate(data: { [s: string]: any }, field: string, value: Array<any>): Promise<boolean> {
         const [anotherField, anotherValue] = value;
 
-        return (
-            typeof data[anotherField] === 'undefined' ||
-            data[anotherField] != anotherValue ||
-            typeof data[field] !== 'undefined' && typeof data[anotherField] !== 'undefined' && data[anotherField] == anotherValue
-        );
+        if (this.checkForUndefined(data, anotherField)) {
+            return false;
+        }
+
+        if (this.checkForUndefined(data, field)) {
+            return false;
+        }
+
+        if (this.compareValues(data[anotherField], anotherValue)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private compareValues(value: any, anotherValue: any): boolean {
+
+        let convertedValue = value;
+        let convertedAnotherValue = anotherValue;
+
+        if (typeof value === 'object' && value !== null) {
+            convertedValue = JSON.stringify(value);
+        }
+        if (typeof anotherValue === 'object' && anotherValue !== null) {
+            convertedAnotherValue = JSON.stringify(anotherValue);
+        }
+
+        if (typeof value === 'number') {
+            convertedValue = value.toString();
+        }
+        if (typeof anotherValue === 'number') {
+            convertedAnotherValue = anotherValue.toString();
+        }
+
+        if (typeof value === 'boolean') {
+            convertedValue = value ? 'true' : 'false';
+        }
+        if (typeof anotherValue === 'boolean') {
+            convertedAnotherValue = anotherValue ? 'true' : 'false';
+        }
+
+        return convertedValue === convertedAnotherValue;
+    }
+
+    private checkForUndefined(data: { [s: string]: any }, field: string): boolean {
+        return typeof data[field] === 'undefined' || data[field] === null;
     }
 
     message(field: string, message: string = '', value: Array<string>): { name: string, message: string } {
