ac79017565531dacf5ffe77d04b96e6556f879bb
Donnie <donniepask@gmail.com>
2025-04-02 20:04:59 +0200

    removed array format for rule

--- src/contracts/IRuleObject.ts
+++ src/contracts/IRuleObject.ts
@@ -1,7 +1,7 @@
 import BaseRule from "../base/BaseRule.js"
 
 export interface IRuleObject {
-    body?: { [k: string]: string | (string | Function | BaseRule | [BaseRule, string, any])[] }
-    params?: { [k: string]: string | (string | Function | BaseRule | [BaseRule, string, any])[] }
-    query?: { [k: string]: string | (string | Function | BaseRule | [BaseRule, string, any])[] }
+    body?: { [k: string]: string | (string | Function | BaseRule)[] }
+    params?: { [k: string]: string | (string | Function | BaseRule)[] }
+    query?: { [k: string]: string | (string | Function | BaseRule)[] }
 }
